5551c164dc8bce2a2ec00dc672f6c1d96adfc424
Andrian <andrisian969@gmail.com>
2025-07-18 15:53:44 +0700

    Coding camp Revou

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-console.log('JavaScript is working!');
 
-// showWelcomePopup();
+
+showWelcomePopup();
 
 function showWelcomePopup() {
     let userName = prompt("Please enter namine Panjenengan:");
+    // If the user clicks "Cancel", userName will be null
     if (userName != ''){
         document.getElementById('welcome-user').innerHTML = userName;
+    }
+}
+
+function validateForm() {
+    const nameInput = document.getElementById('name-input');
+    
+
+    if (nameInput.value === '') {
+        alert('Please enter namine Panjenengan')
+    } else {
+        document.getElementById('message-output').innerHTML = 'Matur sembah nuwon, ${name-Input} sampun diisi';
+        nameInput.value = ''; // Clear the input field after submission 
     }
 }
